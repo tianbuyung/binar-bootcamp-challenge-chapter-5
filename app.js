f2694d5ad6286af8464ad9dbc1937b13dddc4cb7
Septian Maulana <septianm028@gmail.com>
2022-07-02 01:17:01 +0700

    [tianbuyung] - created app with express generator, created models, controllers, error middleware, dotenv and license

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+// import middleware
+const clientError = require("./middleware/clientError");
+const serverError = require("./middleware/serverError");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use(clientError.client);
+app.use(serverError.server);
+
+require("dotenv").config();
+
+module.exports = app;
